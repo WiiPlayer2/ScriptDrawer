567750d246a0a3ea6e552c712b3a9c1cfb303df6
Waldemar Tomme <waldetomme@gmail.com>
2022-09-24 12:06:56 +0200

    Improve pipeline interface

--- ScriptDrawer.Shared/IPipeline.cs
+++ ScriptDrawer.Shared/IPipeline.cs
@@ -2,5 +2,17 @@
 
 public interface IPipeline
 {
-    public Task ExecuteAsync(IPublisher publisher);
+    Type ConfigurationType { get; }
+
+    Task ExecuteAsync(IPublisher publisher, object? configuration);
+}
+
+public interface IPipeline<in TConfiguration> : IPipeline
+{
+    Type IPipeline.ConfigurationType => typeof(TConfiguration);
+
+    Task IPipeline.ExecuteAsync(IPublisher publisher, object? configuration)
+        => ExecuteAsync(publisher, (TConfiguration) configuration!);
+
+    Task ExecuteAsync(IPublisher publisher, TConfiguration configuration);
 }
