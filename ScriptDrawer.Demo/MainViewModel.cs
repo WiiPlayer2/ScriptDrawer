82a4860812ed5eda1007027cfce931a9b39a971b
Waldemar Tomme <waldetomme@gmail.com>
2022-09-24 21:06:17 +0200

    Cleanup pipeline

--- ScriptDrawer.Demo/MainViewModel.cs
+++ ScriptDrawer.Demo/MainViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Text;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ReactiveUI;
 using ScriptDrawer.Core;
@@ -18,55 +19,12 @@ internal class MainViewModel
 {
     private readonly Subject<Config> configSubject = new();
 
-    public MainViewModel(IOptionsMonitor<Config> configMonitor)
+    public MainViewModel(IOptionsMonitor<Config> configMonitor, ILogger<MainViewModel> logger)
     {
         var publisher = new DelegatePublisher((name, image) => PublishedImages.Add(new PublishedImage(name, image)));
 
-        configSubject
-            .Select(config => config.Pipeline)
-            .Select(pipelineFile => Observable.Create<string>(observer =>
-            {
-                observer.OnNext(pipelineFile);
-
-                var fullPath = Path.GetFullPath(pipelineFile);
-                var directoryName = Path.GetDirectoryName(pipelineFile)!;
-
-                var fileSystemWatcher = new FileSystemWatcher(directoryName);
-                fileSystemWatcher.EnableRaisingEvents = true;
-                var subscription = Observable.FromEventPattern<FileSystemEventArgs>(fileSystemWatcher, nameof(fileSystemWatcher.Changed))
-                    .Select(o => o.EventArgs.FullPath)
-                    .Where(o => Path.GetFullPath(o) == fullPath)
-                    .Subscribe(observer);
-
-                return () =>
-                {
-                    subscription.Dispose();
-                    fileSystemWatcher.Dispose();
-                };
-            }).Retry())
-            .Switch()
-            .Where(File.Exists)
-            .SelectAsync(async pipelineFile =>
-            {
-                try
-                {
-                    var code = await File.ReadAllTextAsync(pipelineFile);
-                    var scriptOptions = ScriptOptions.Default
-                        .WithFilePath(Path.GetFullPath(pipelineFile))
-                        .WithFileEncoding(Encoding.UTF8)
-                        .WithReferences(typeof(IPipeline).Assembly)
-                        .WithEmitDebugInformation(true);
-                    var script = CSharpScript.Create<Type>(code, scriptOptions);
-                    var pipelineType = (await script.RunAsync()).ReturnValue;
-                    return (IPipeline) Activator.CreateInstance(pipelineType)!;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    return null;
-                }
-            })
-            .WhereNotNull()
+        GetPipelineCode(configSubject.Select(config => config.Pipeline))
+            .Compile(logger)
             .ObserveOnDispatcher()
             .Subscribe(async pipeline =>
             {
@@ -77,7 +35,7 @@ internal class MainViewModel
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e);
+                    logger.LogError(e, "Pipeline failed.");
                 }
             });
 
@@ -86,6 +44,56 @@ internal class MainViewModel
     }
 
     public ObservableCollection<PublishedImage> PublishedImages { get; } = new();
+
+    private IObservable<string> GetPipelineCode(IObservable<string> pipelineFile)
+        => pipelineFile
+            .Select(file => WatchFile(file).SelectAsync(_ => File.ReadAllTextAsync(file)))
+            .Switch();
+
+    private IObservable<Unit> WatchFile(string file)
+        => Observable.Create<Unit>(observer =>
+        {
+            observer.OnNext(Unit.Default);
+
+            var fullPath = Path.GetFullPath(file);
+            var directoryName = Path.GetDirectoryName(file)!;
+
+            var fileSystemWatcher = new FileSystemWatcher(directoryName);
+            fileSystemWatcher.EnableRaisingEvents = true;
+            var subscription = Observable.FromEventPattern<FileSystemEventArgs>(fileSystemWatcher, nameof(fileSystemWatcher.Changed))
+                .Select(o => o.EventArgs.FullPath)
+                .Where(o => Path.GetFullPath(o) == fullPath && File.Exists(fullPath))
+                .Select(_ => Unit.Default)
+                .Subscribe(observer);
+
+            return () =>
+            {
+                subscription.Dispose();
+                fileSystemWatcher.Dispose();
+            };
+        });
+}
+
+internal static class RenderExtensions
+{
+    public static IObservable<IPipeline> Compile(this IObservable<string> code, ILogger logger)
+        => code.SelectAsync(async code =>
+            {
+                try
+                {
+                    var scriptOptions = ScriptOptions.Default
+                        .WithReferences(typeof(IPipeline).Assembly);
+                    var script = CSharpScript.Create<Type>(code, scriptOptions);
+                    var pipelineType = (await script.RunAsync()).ReturnValue;
+                    return (IPipeline) Activator.CreateInstance(pipelineType)!;
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "Failed to compile pipeline.");
+                    return null;
+                }
+            })
+            .WhereNotNull();
 }
 
 internal record PublishedImage(string Name, Image Image);
