10ab4c1aa0ad5aaa64135c9f6ecc096477613b14
Waldemar Tomme <waldetomme@gmail.com>
2022-09-25 16:13:38 +0200

    Implement CLI logic

--- ScriptDrawer.Cli/Program.cs
+++ ScriptDrawer.Cli/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.CommandLine;
+using ScriptDrawer.Core;
+using SixLabors.ImageSharp;
+using YamlDotNet.Serialization;
 
 var pipelineFileOption = new Option<FileInfo>(
     "--pipeline-file",
@@ -26,4 +29,28 @@ var rootCommand = new RootCommand("CLI tool to execute ScriptDrawer pipelines.")
 rootCommand.SetHandler(Run, pipelineFileOption, configFileOption, outputDirectoryOption);
 return await rootCommand.InvokeAsync(args);
 
-Task Run(FileInfo pipelineFile, FileInfo? configFile, DirectoryInfo outputDirectory) => throw new NotImplementedException();
+async Task Run(FileInfo pipelineFile, FileInfo? configFile, DirectoryInfo outputDirectory)
+{
+    var engine = new Engine();
+    var pipelineCode = await File.ReadAllTextAsync(pipelineFile.FullName);
+    var pipeline = await engine.CompilePipelineAsync(pipelineCode, CancellationToken.None);
+    var configuration = await ParseConfiguration(configFile, pipeline.ConfigurationType);
+    var publisher = new DelegatePublisher((name, image, cancellationToken) => SaveImage(outputDirectory, name, image, cancellationToken));
+    await pipeline.ExecuteAsync(publisher, configuration, CancellationToken.None);
+}
+
+async Task SaveImage(DirectoryInfo outputDirectory, string name, Image image, CancellationToken cancellationToken)
+{
+    outputDirectory.Create();
+    var targetPath = Path.Combine(outputDirectory.FullName, $"{name}.png");
+    await image.SaveAsPngAsync(targetPath, cancellationToken);
+}
+
+async Task<object?> ParseConfiguration(FileInfo? configFile, Type configurationType)
+{
+    if (configFile is null) return null;
+    var deserializer = new DeserializerBuilder().Build();
+    var content = await File.ReadAllTextAsync(configFile.FullName);
+    var configuration = deserializer.Deserialize(content, configurationType);
+    return configuration;
+}
