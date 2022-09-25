c985f9269bb4185b4e7216f5f210132d5a13c62e
Waldemar Tomme <waldetomme@gmail.com>
2022-09-25 22:54:33 +0200

    Use hosting in CLI

--- ScriptDrawer.Cli/Program.cs
+++ ScriptDrawer.Cli/Program.cs
@@ -1,56 +1,56 @@
 ﻿using System.CommandLine;
-using ScriptDrawer.Core;
-using SixLabors.ImageSharp;
-using YamlDotNet.Serialization;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ScriptDrawer.Cli;
 
-var pipelineFileOption = new Option<FileInfo>(
-    "--pipeline-file",
-    "The pipeline file to use.")
+return await BuildCommandLine()
+    .UseHost(
+        _ => Host.CreateDefaultBuilder(),
+        host => host
+            .ConfigureServices(ConfigureServices))
+    .UseDefaults()
+    .Build()
+    .InvokeAsync(args);
+
+void ConfigureServices(IServiceCollection services)
 {
-    IsRequired = true,
-};
-
-var configFileOption = new Option<FileInfo?>(
-    "--config-file",
-    "The configuration file to use.");
-
-var outputDirectoryOption = new Option<DirectoryInfo>(
-    "--output-directory",
-    () => new DirectoryInfo(Environment.CurrentDirectory),
-    "The directory in which all published images are put.");
-
-var rootCommand = new RootCommand("CLI tool to execute ScriptDrawer pipelines.")
-{
-    pipelineFileOption,
-    configFileOption,
-    outputDirectoryOption,
-};
-
-rootCommand.SetHandler(Run, pipelineFileOption, configFileOption, outputDirectoryOption);
-return await rootCommand.InvokeAsync(args);
-
-async Task Run(FileInfo pipelineFile, FileInfo? configFile, DirectoryInfo outputDirectory)
-{
-    var engine = new Engine();
-    var pipelineCode = await File.ReadAllTextAsync(pipelineFile.FullName);
-    var pipeline = await engine.CompilePipelineAsync(pipelineCode, CancellationToken.None);
-    var configuration = await ParseConfiguration(configFile, pipeline.ConfigurationType);
-    var publisher = new DelegatePublisher((name, image, cancellationToken) => SaveImage(outputDirectory, name, image, cancellationToken));
-    await pipeline.ExecuteAsync(publisher, configuration, CancellationToken.None);
+    services.AddScriptDrawer();
+    services.AddSingleton<Tool>();
 }
 
-async Task SaveImage(DirectoryInfo outputDirectory, string name, Image image, CancellationToken cancellationToken)
+CommandLineBuilder BuildCommandLine()
 {
-    outputDirectory.Create();
-    var targetPath = Path.Combine(outputDirectory.FullName, $"{name}.png");
-    await image.SaveAsPngAsync(targetPath, cancellationToken);
+    var pipelineFileOption = new Option<FileInfo>(
+        "--pipeline-file",
+        "The pipeline file to use.")
+    {
+        IsRequired = true,
+    };
+
+    var configFileOption = new Option<FileInfo?>(
+        "--config-file",
+        "The configuration file to use.");
+
+    var outputDirectoryOption = new Option<DirectoryInfo>(
+        "--output-directory",
+        () => new DirectoryInfo(Environment.CurrentDirectory),
+        "The directory in which all published images are put.");
+
+    var rootCommand = new RootCommand("CLI tool to execute ScriptDrawer pipelines.")
+    {
+        pipelineFileOption,
+        configFileOption,
+        outputDirectoryOption,
+    };
+
+    rootCommand.Handler = CommandHandler.Create<Input, IHost>(
+        (input, host) => host.Services.GetRequiredService<Tool>().Run(input.PipelineFile, input.ConfigFile, input.OutputDirectory));
+
+    return new CommandLineBuilder(rootCommand);
 }
 
-async Task<object?> ParseConfiguration(FileInfo? configFile, Type configurationType)
-{
-    if (configFile is null) return null;
-    var deserializer = new DeserializerBuilder().Build();
-    var content = await File.ReadAllTextAsync(configFile.FullName);
-    var configuration = deserializer.Deserialize(content, configurationType);
-    return configuration;
-}
+internal record Input(FileInfo PipelineFile, FileInfo? ConfigFile, DirectoryInfo OutputDirectory);
---
+++ ScriptDrawer.Cli/Tool.cs
@@ -0,0 +1,42 @@
+﻿using ScriptDrawer.Core;
+using ScriptDrawer.Serialization;
+using SixLabors.ImageSharp;
+
+namespace ScriptDrawer.Cli;
+
+internal class Tool
+{
+    private readonly ConfigSerializer configSerializer;
+
+    private readonly Engine engine;
+
+    public Tool(Engine engine, ConfigSerializer configSerializer)
+    {
+        this.engine = engine;
+        this.configSerializer = configSerializer;
+    }
+
+    private async Task<object?> ParseConfiguration(FileInfo? configFile, Type configurationType)
+    {
+        if (configFile is null) return null;
+        var content = await File.ReadAllTextAsync(configFile.FullName);
+        var configuration = configSerializer.Deserialize(content, configurationType);
+        return configuration;
+    }
+
+    public async Task Run(FileInfo pipelineFile, FileInfo? configFile, DirectoryInfo outputDirectory)
+    {
+        var pipelineCode = await File.ReadAllTextAsync(pipelineFile.FullName);
+        var pipeline = await engine.CompilePipelineAsync(pipelineCode, CancellationToken.None);
+        var configuration = await ParseConfiguration(configFile, pipeline.ConfigurationType);
+        var publisher = new DelegatePublisher((name, image, cancellationToken) => SaveImage(outputDirectory, name, image, cancellationToken));
+        await pipeline.ExecuteAsync(publisher, configuration, CancellationToken.None);
+    }
+
+    private async Task SaveImage(DirectoryInfo outputDirectory, string name, Image image, CancellationToken cancellationToken)
+    {
+        outputDirectory.Create();
+        var targetPath = Path.Combine(outputDirectory.FullName, $"{name}.png");
+        await image.SaveAsPngAsync(targetPath, cancellationToken);
+    }
+}
