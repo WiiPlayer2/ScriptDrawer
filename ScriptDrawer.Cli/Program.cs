622b49d57bd4252ad7f5bd56ab4f19c7914ec0ae
Waldemar Tomme <waldetomme@gmail.com>
2022-09-25 15:57:21 +0200

    Add cli project

---
+++ ScriptDrawer.Cli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.CommandLine;
+
+var pipelineFileOption = new Option<FileInfo>(
+    "--pipeline-file",
+    "The pipeline file to use.")
+{
+    IsRequired = true,
+};
+
+var configFileOption = new Option<FileInfo?>(
+    "--config-file",
+    "The configuration file to use.");
+
+var outputDirectoryOption = new Option<DirectoryInfo>(
+    "--output-directory",
+    () => new DirectoryInfo(Environment.CurrentDirectory),
+    "The directory in which all published images are put.");
+
+var rootCommand = new RootCommand("CLI tool to execute ScriptDrawer pipelines.")
+{
+    pipelineFileOption,
+    configFileOption,
+    outputDirectoryOption,
+};
+
+rootCommand.SetHandler(Run, pipelineFileOption, configFileOption, outputDirectoryOption);
+return await rootCommand.InvokeAsync(args);
+
+Task Run(FileInfo pipelineFile, FileInfo? configFile, DirectoryInfo outputDirectory) => throw new NotImplementedException();
