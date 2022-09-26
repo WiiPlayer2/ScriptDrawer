331058f1528812926ceb7283569fa053ea115df5
Waldemar Tomme <waldetomme@gmail.com>
2022-09-26 19:44:03 +0200

    Add logging to cli tool

--- ScriptDrawer.Cli/Tool.cs
+++ ScriptDrawer.Cli/Tool.cs
@@ -1,4 +1,5 @@
-﻿using ScriptDrawer.Core;
+﻿using Microsoft.Extensions.Logging;
+using ScriptDrawer.Core;
 using ScriptDrawer.Serialization;
 using SixLabors.ImageSharp;
 
@@ -10,15 +11,20 @@ internal class Tool
 
     private readonly Engine engine;
 
-    public Tool(Engine engine, ConfigSerializer configSerializer)
+    private readonly ILogger<Tool> logger;
+
+    public Tool(Engine engine, ConfigSerializer configSerializer, ILogger<Tool> logger)
     {
         this.engine = engine;
         this.configSerializer = configSerializer;
+        this.logger = logger;
     }
 
     private async Task<PipelineConfig?> ParseConfiguration(FileInfo? configFile, Type configurationType)
     {
         if (configFile is null) return null;
+
+        logger.LogInformation("Reading configuration...");
         var content = await File.ReadAllTextAsync(configFile.FullName);
         var configuration = configSerializer.Deserialize(content, configurationType);
         return configuration;
@@ -26,10 +32,13 @@ internal class Tool
 
     public async Task Run(Input input)
     {
+        logger.LogInformation("Compiling pipeline...");
         var pipelineCode = await File.ReadAllTextAsync(input.PipelineFile.FullName);
         var pipeline = await engine.CompilePipelineAsync(pipelineCode, CancellationToken.None);
+
         var configuration = await ParseConfiguration(input.ConfigFile, pipeline.ConfigurationType);
 
+        logger.LogInformation("Executing pipeline...");
         var publisher = new DelegatePublisher((name, image, cancellationToken) => SaveImage(input.OutputDirectory, name, image, cancellationToken));
         await engine.RunPipelineAsync(pipeline, configuration, publisher, CancellationToken.None);
     }
@@ -38,6 +47,8 @@ internal class Tool
     {
         outputDirectory.Create();
         var targetPath = Path.Combine(outputDirectory.FullName, $"{name}.png");
+
+        logger.LogInformation("Exporting {name} to {path}...", name, targetPath);
         await image.SaveAsPngAsync(targetPath, cancellationToken);
     }
 }
